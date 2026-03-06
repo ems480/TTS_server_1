420065d0ab987e91ff554260fad4605ea2dbaf68
ems480 <116377770+ems480@users.noreply.github.com>
2026-03-06 07:13:31 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import hashlib
 
 def generate_hash(text: str, voice: str = "en-US-AvaNeural", rate: str = "-15%", pitch: str = "-5Hz"):
+    # Normalize text
     text = text.strip().lower()
     combined = f"{text}_{voice}_{rate}_{pitch}"
     return hashlib.sha256(combined.encode()).hexdigest()
