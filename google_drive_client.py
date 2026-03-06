9e65e5e10b3215b2f73d0eb0f9ef92d626ae0b3b
ems480 <116377770+ems480@users.noreply.github.com>
2026-03-06 07:14:21 +0200

    Update google_drive_client.py

--- google_drive_client.py
+++ google_drive_client.py
@@ -1,17 +1,17 @@
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
-# Setup authentication
+# Authenticate Google Drive once (first time only)
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()  # first time only, follow browser login
+gauth.LocalWebserverAuth()  # Opens browser for login
 drive = GoogleDrive(gauth)
 
 def upload_file_to_drive(local_file, title):
     file_drive = drive.CreateFile({'title': title})
     file_drive.SetContentFile(local_file)
     file_drive.Upload()
-    
-    # Make the file public and get the link
+
+    # Make file publicly accessible
     file_drive.InsertPermission({
         'type': 'anyone',
         'value': 'anyone',
