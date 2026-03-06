021759e8163698b39e4ca857bedad9ffe3b0e27d
ems480 <116377770+ems480@users.noreply.github.com>
2026-03-06 06:48:33 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String
+from database import Base
+
+class Audio(Base):
+    __tablename__ = "audio"
+
+    id = Column(String, primary_key=True)
+    text_hash = Column(String, unique=True)
+    title = Column(String)
+    google_drive_url = Column(String)
